5ea14e815f50a053e26e61aca6360c7980a340c8
Jonny Pickard <jonathandpickard@gmail.com>
2017-02-26 18:32:00 +0000

    Add initial front end
    Add index.js with ajax call to POST /till/scan-item

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,18 @@
-console.log('test');
+document.getElementById('addItem').onclick = function() {
+  var form = new FormData(document.getElementById('tillForm'));
+  fetch('/till/scan-item', {
+    method: 'POST',
+    body: form
+  }).then(function(res) {
+    return res.json().then(function(json) {
+      var htmlString = '';
+      for (var i = 0; i < json[0].length; i++) {
+        htmlString +=
+        '<li class="list-group-item">' +
+          json[0][i][0] + ': £' + json[0][i][1] +
+        '</li> ';
+      }
+      document.getElementById('basketList').innerHTML = htmlString;
+    });
+  });
+};
--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 const till = require('./app/till');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const multer = require('multer');
+const upload = multer();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(morgan('dev'));
 
-app.post('/till/scan-item', (req, res) =>{
+app.post('/till/scan-item', upload.fields([]), (req, res) =>{
   const item = req.body.item;
   const basket = till.scanItem(item);
-  res.send(basket);
+  console.log(basket);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(basket));
 });
 
 app.get('/till/checkout', (req, res) =>{
